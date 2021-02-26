81307e15d59f28cd1a1cf5583ad227b89fdd3bb1
Rafael Abrantes <rafael.vieira.abranttes@gmail.com>
2021-02-26 02:59:05 -0300

    Retirando erro de incialização

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lojavirtualestudo/screens/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,11 +9,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Loja de Roupas',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        primaryColor: Color.fromARGB(255, 4, 125, 141),
       ),
+      home: HomeScreen(),
     );
   }
 }
