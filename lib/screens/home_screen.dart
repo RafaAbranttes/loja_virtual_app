b45b8091a45cdcfc1c6cea0709fe194cd234672b
Rafael Abrantes <rafael.vieira.abranttes@gmail.com>
2021-03-01 21:21:40 -0300

    construção do drawer

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lojavirtualestudo/tabs/home_tab.dart';
+import 'package:lojavirtualestudo/widgets/custom_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
   final _pageController = PageController();
@@ -10,7 +11,10 @@ class HomeScreen extends StatelessWidget {
       controller: _pageController,
       physics: NeverScrollableScrollPhysics(),
       children: <Widget>[
-        HomeTab(),
+        Scaffold(
+          body: HomeTab(),
+          drawer: CustomDrawer(),
+        ),
       ],
     );
   }
