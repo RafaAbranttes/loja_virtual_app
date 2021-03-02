f11ff07bd0e583418220d9f3b4aee7a59139b230
Rafael Abrantes <rafael.vieira.abranttes@gmail.com>
2021-03-01 21:47:39 -0300

    criação da navegação pelo drawer

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,7 +13,16 @@ class HomeScreen extends StatelessWidget {
       children: <Widget>[
         Scaffold(
           body: HomeTab(),
-          drawer: CustomDrawer(),
+          drawer: CustomDrawer(_pageController),
+        ),
+        Container(
+          color: Colors.red,
+        ),
+        Container(
+          color: Colors.yellow,
+        ),
+        Container(
+          color: Colors.blue,
         ),
       ],
     );
